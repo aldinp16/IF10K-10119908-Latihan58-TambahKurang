19038a8055eb62d7a93dfb3d1225d22f69e505f2
Aldi Nugraha <aldinugrahap16@gmail.com>
2020-11-22 09:08:39 +0800

    first commit

---
+++ src/if10k/pkg10119908/latihan58/tambahkurang/IF10K10119908Latihan58TambahKurang.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119908.latihan58.tambahkurang;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama     : Aldi Nugraha
+ * NIM      : 10119908
+ * Kelas    : IF-10K
+ */
+public class IF10K10119908Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jb = new JumlahBilangan(3, 4);
+        jb.tampilHasilJumlah();
+        SelisihBilangan sb= new SelisihBilangan(3, 4);
+        sb.tampilSelisih();
+    }
+    
+}
---
+++ src/if10k/pkg10119908/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119908.latihan58.tambahkurang;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama     : Aldi Nugraha
+ * NIM      : 10119908
+ * Kelas    : IF-10K
+ */
+public class JumlahBilangan extends Bilangan{
+
+    public JumlahBilangan(int x, int y) {
+        super(x, y);
+    }
+   
+    public void tampilHasilJumlah(){
+        System.out.println("Hasil Perjumlahan = " + (getX()+getY()));
+    }
+}
